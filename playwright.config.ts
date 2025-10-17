16d8834c8c442890fa717a42b84bb3e721b0fa4d
MontassarBenH <montassar.hmida@stud.hs-emden-leer.de>
2025-10-17 19:51:36 +0200

    CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,20 @@
-// playwright.config.ts
+
 import { defineConfig } from '@playwright/test';
 
-const isCI = !!process.env.CI;
-const baseURL = process.env.E2E_BASE_URL || 'http://localhost:3000';
-const isLocal = baseURL.startsWith('http://localhost');
+const isRemote = !!process.env.E2E_BASE_URL;
 
 export default defineConfig({
   testDir: 'tests/e2e',
   use: {
-    baseURL,
+    baseURL: process.env.E2E_BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
   },
-  webServer: isLocal && !isCI
-    ? {
+  webServer: isRemote
+    ? undefined 
+    : {
         command: 'npm run dev',
         url: 'http://localhost:3000',
         reuseExistingServer: true,
         timeout: 120_000,
-      }
-    : undefined,
+      },
 });
