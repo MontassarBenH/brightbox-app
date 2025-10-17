ebc048437924f1ab1a366a735dac5a3b81a26b17
MontassarBenH <montassar.hmida@stud.hs-emden-leer.de>
2025-10-17 20:44:28 +0200

    CI Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,17 @@
-
 import { defineConfig } from '@playwright/test';
 
-const isRemote = !!process.env.E2E_BASE_URL;
+const isLive = process.env.PLAYWRIGHT_NO_SERVER === '1';
 
 export default defineConfig({
   testDir: 'tests/e2e',
+
   use: {
     baseURL: process.env.E2E_BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
   },
-  webServer: isRemote
-    ? undefined 
+
+  webServer: isLive
+    ? undefined
     : {
         command: 'npm run dev',
         url: 'http://localhost:3000',
